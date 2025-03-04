17dc129c10b4352609a98fada016043527dbe1ab
Laura <laurarodriguezbasanta@gmail.com>
2025-03-04 17:51:39 +0100

    Creo las entidades y realizo las migraciones

---
+++ backend/src/Entity/Application.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ApplicationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ApplicationRepository::class)]
+class Application
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $coverLetter = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCoverLetter(): ?string
+    {
+        return $this->coverLetter;
+    }
+
+    public function setCoverLetter(?string $coverLetter): static
+    {
+        $this->coverLetter = $coverLetter;
+
+        return $this;
+    }
+}
---
+++ backend/src/Entity/JobOffer.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JobOfferRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JobOfferRepository::class)]
+class JobOffer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $loation = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $salary = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLoation(): ?string
+    {
+        return $this->loation;
+    }
+
+    public function setLoation(string $loation): static
+    {
+        $this->loation = $loation;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getSalary(): ?string
+    {
+        return $this->salary;
+    }
+
+    public function setSalary(string $salary): static
+    {
+        $this->salary = $salary;
+
+        return $this;
+    }
+}
