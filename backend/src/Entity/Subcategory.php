ca72a3327c02a62f119e5aa552e6c3d8c6fe772d
Laura <laurarodriguezbasanta@gmail.com>
2025-03-10 05:27:54 +0100

    creo la relación entre categoría y subcategoría y hago la migración

--- backend/src/Entity/Subcategory.php
+++ backend/src/Entity/Subcategory.php
@@ -16,6 +16,13 @@ class Subcategory
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+     //------Relación con categoría. Muchos a uno, una subcategoría pertenece a una sola categoría
+     #[ORM\ManyToOne(targetEntity: Category::class, inversedBy:'subcategories')]
+     #[ORM\JoinColumn(nullable: false)]
+     private ?Category $category = null;
+
+
+
     public function getId(): ?int
     {
         return $this->id;
@@ -32,4 +39,19 @@ class Subcategory
 
         return $this;
     }
+
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+    
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+        return $this;
+    }
+
+
+
 }
