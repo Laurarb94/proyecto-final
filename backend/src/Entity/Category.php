2c4989305ec47bbae6812c056bf23928a20a6443
Laura <laurarodriguezbasanta@gmail.com>
2025-03-10 05:27:54 +0100

    creo la relación entre categoría y subcategoría y hago la migración

--- backend/src/Entity/Category.php
+++ backend/src/Entity/Category.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -15,6 +17,18 @@ class Category
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+
+    //------Relación con subcategory. Uno a muchos, una categoría puede tener muchas subcategorías, pero cada subcategoría pertenece sólo a 1 categoría
+    #[ORM\OneToMany(mappedBy:'category', targetEntity: Subcategory::class, orphanRemoval: true)]
+    private Collection $subcategories;
+
+
+    public function __construct()
+    {
+        $this->subcategories = new ArrayCollection();
+    }
+
 
     public function getId(): ?int
     {
@@ -32,4 +46,34 @@ class Category
 
         return $this;
     }
+
+    //-----Métodos relación con subcategoria
+    public function getSubcategories(): Collection
+    {
+        return $this->subcategories;
+    }
+
+    public function addSubcategory(Subcategory $subcategory): self
+    {
+        if (!$this->subcategories->contains($subcategory)) {
+            $this->subcategories[] = $subcategory;
+            $subcategory->setCategory($this);
+        }
+        
+        return $this;
+    }
+    
+    public function removeSubcategory(Subcategory $subcategory): self
+    {
+        if ($this->subcategories->removeElement($subcategory)) {
+            if ($subcategory->getCategory() === $this) {
+                $subcategory->setCategory(null);
+            }
+        }
+
+    return $this;
+
+    }
+
+
 }
