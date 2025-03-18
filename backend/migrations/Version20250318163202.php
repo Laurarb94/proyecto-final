57764f3a31ee8e6c31009f58d57cc1a553120f72
Laura <laurarodriguezbasanta@gmail.com>
2025-03-18 19:14:07 +0100

    Comienzo con el controlador usuarios: método listar y crear funcionan en postman

---
+++ backend/migrations/Version20250318163202.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250318163202 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB95DC6FE57 FOREIGN KEY (subcategory_id) REFERENCES subcategory (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE course DROP FOREIGN KEY FK_169E6FB95DC6FE57');
+    }
+}
