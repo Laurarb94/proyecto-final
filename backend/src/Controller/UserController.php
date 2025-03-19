fa555454d95a46b3dd8a98992a0969f3437d4465
Laura <laurarodriguezbasanta@gmail.com>
2025-03-19 15:57:21 +0100

    añado métodos editar y borrar, funcionan en postman. Ya tengo crud de usuarios fucnional para back

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -19,7 +19,7 @@ final class UserController extends AbstractController{
 
     private $em;
 
-    public function __construct(EntityManagerInterface $em, ParameterBagInterface $params)
+    public function __construct(EntityManagerInterface $em)
     {
         $this->em = $em;
     }
@@ -98,20 +98,105 @@ final class UserController extends AbstractController{
 
     }
 
-    /*-----------------------------------------Editar usuarios----------------------------------------------------------------------------------------
+    //-----------------------------------------Editar usuarios----------------------------------------------------------------------------------------
     #[Route('api/user/{id}', name:'user_edit', methods:['PUT'])]
-    public function edit(int $id, Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    public function edit(int $id, Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): JsonResponse
     {
-        
+        //obtienes el usuario por su id. Con find(id)->te va a devolver un solo objeto, un solo usuario con ese id
+        $user = $em->getRepository(User::class)->find($id);
+
+        if(!$user){
+            return new JsonResponse(['error'=>'Usuario no encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        //Actualizas los datos del usuario:
+        if(isset($data['name'])){
+            $user->setName($data['name']);
+        }
+
+        if(isset($data['last_name1'])){
+            $user->setLastName1($data['last_name1']);
+        }
+
+        if(isset($data['last_name2'])){
+            $user->setLastName2($data['last_name2']);
+        }
+
+        if(isset($data['email'])){
+            $user->setEmail($data['email']);
+        }
+
+        if(isset($data['password'])){
+            $user->setPassword($data['password']);
+        }
+
+        if(isset($data['country'])){
+            $user->setCountry($data['country']);
+        }
+
+        if(isset($data['city'])){
+            $user->setCity($data['city']);
+        }
+
+        if(isset($data['phone'])){
+            $user->setPhone($data['phone']);
+        }
+
+        if(isset($data['photo'])){
+            $user->setPhoto($data['photo']);
+        }
+
+        if(isset($data['biography'])){
+            $user->setBiography($data['biography']);
+        }
+
+        if(isset($data['roles'])){
+            $user->setRoles($data['roles']);
+        }
+
+        //Guardar los datos cambiados en la bbdd
+        $em->flush();
+
+        //Responder con los datos del usuario actualizado
+        return $this->json([
+            'id'=> $user->getId(),
+            'name'=>$user->getName(),
+            'las_name1' => $user->getLastName1(),
+            'last_name2'=>$user->getLastName2(),
+            'email'=>$user->getEmail(),
+            'password'=>$user->getPassword(),
+            'country'=>$user->getCountry(),
+            'city'=>$user->getCity(),
+            'phone'=>$user->getPhone(),
+            'photo'=>$user->getPhoto(),
+            'biography'=>$user->getBiography(),
+            'roles'=>$user->getRoles(),
+        ], Response::HTTP_OK);
     }
 
-    //Eliminar usuarios
+    //-------------------------------------------Eliminar usuarios--------------------------------------------------------------------------------------
     #[Route('api/user/{id}', name:'user_delete', methods:['DELETE'])]
-    public function delete(int $id): Response
+    public function delete(int $id, EntityManagerInterface $em): Response
     {
-       
+        $user = $em->getRepository(User::class)->find($id);
+
+       if(!$user){
+        return new JsonResponse(['error'=>'Usuario no encontrado'], Response::HTTP_NOT_FOUND);
+       }
+
+       //Eliminas al usuario
+       $em->remove($user);
+       $em->flush();
+
+       //Respondes con mensaje de éxito
+       return new JsonResponse(['message'=>'Usuario eliminado correctamente'], Response::HTTP_OK);
+
     }
 
+
+    /*
     //Métodos para añadie para el perfil del usuario
 
     //Obtener ofertas de emplo del usuario
